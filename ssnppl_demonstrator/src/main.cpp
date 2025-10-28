f29412f1cd395598416d945560367acfcf87bd02
Lucca Silva <Lucca.silva@nordian.com>
2025-10-28 15:29:02 -0300

    fixes

--- ssnppl_demonstrator/src/main.cpp
+++ ssnppl_demonstrator/src/main.cpp
@@ -201,7 +201,7 @@ int main() {
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     t_sept.join();
